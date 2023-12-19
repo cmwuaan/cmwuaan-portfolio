3a1d8bc53af5227e07a9349676cd8ceb007ba62a
cmwuaan <minhquanrbpk@gmail.com>
2023-12-20 04:44:47 +0700

    fix hero section

---
+++ components/intro.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Intro() {
+  return (
+    <section className="lg:my-44 my-10">
+      <p className="text-2xl sm:text-5xl lg:text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-white to-gray-400">
+        I create elevating digital experiences that inspire and connect with people through development and design.
+      </p>
+    </section>
+  );
+}
