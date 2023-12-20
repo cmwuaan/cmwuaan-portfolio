9c10885bb6587a7c0b3074efadeebd1d58732207
cmwuaan <minhquanrbpk@gmail.com>
2023-12-20 08:53:05 +0700

    scroll behaving adjust

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="!scroll-smooth snap-y snap-mandatory" lang="en">
+    <html className="!scroll-smooth md:snap-y md:snap-mandatory" lang="en">
       <body className={`${inter.className} bg-black text-white`}>
         <Header />
         {children}
